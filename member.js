37d07793c2b3c7b9505fcefe535b05040a6477f3
Mohamed Adam Ata <115686926+CODINATA@users.noreply.github.com>
2024-12-21 10:52:31 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember()  {
+  const skills = [
+    { name: 'HTML', level: 10 },
+    { name: 'CSS', level: 10 },
+    { name: 'JavaScript', level: 8 },
+    { name: 'React', level: 7 },
+    { name: 'Node.js', level: 6 },
+  ];
+  return skills;
+}
